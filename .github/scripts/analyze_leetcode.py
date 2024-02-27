61c27af16a0c6e2133d03efa75781d38de403861
kardeepakkumar <deepakkumaar97@gmail.com>
2024-02-27 12:49:44 +0530

    leetcode-workflow

--- .github/scripts/analyze_leetcode.py
+++ .github/scripts/analyze_leetcode.py
@@ -86,10 +86,12 @@ stats_summary = f"""## LeetCode Statistics Summary
 """
 
 update_readme_statistics(readme_path, stats_summary)
+
 def defaultdict_to_dict(d):
     if isinstance(d, defaultdict):
         d = {k: defaultdict_to_dict(v) for k, v in d.items()}
     return d
+date_count_dict = defaultdict_to_dict(stats['date_count'])
 
 with open(data_json_path, 'w') as json_file:
     json.dump({"date_count": date_count_dict}, json_file, indent=4)
